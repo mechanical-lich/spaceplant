abff388a3e9ff4673b2577e3ff4d11ef8d7bd6ec
M.Lich <mechanical.lich@gmail.com>
2026-03-23 19:20:56 -0400

    Move doors to the blueprint file and don't manually build them

--- internal/generation/doors.go
+++ internal/generation/doors.go
@@ -1,10 +1,8 @@
 package generation
 
 import (
-	"github.com/mechanical-lich/ml-rogue-lib/pkg/rlcomponents"
-	"github.com/mechanical-lich/mlge/ecs"
 	"github.com/mechanical-lich/spaceplant/internal/component"
-	"github.com/mechanical-lich/spaceplant/internal/config"
+	"github.com/mechanical-lich/spaceplant/internal/factory"
 	"github.com/mechanical-lich/spaceplant/internal/world"
 )
 
@@ -12,48 +10,22 @@ import (
 // The underlying tile is set to floor so the entity provides all blocking/visual.
 func spawnDoor(l *world.Level, x, y, z int) {
 	l.SetTileTypeAt(x, y, z, world.TypeFloor)
-
-	e := &ecs.Entity{}
-	pc := &rlcomponents.PositionComponent{}
-	pc.SetPosition(x, y, z)
-	e.AddComponent(pc)
-	e.AddComponent(&rlcomponents.SolidComponent{})
-	e.AddComponent(&rlcomponents.DoorComponent{
-		ClosedSpriteX: 11 * config.SpriteWidth,
-		ClosedSpriteY: 0,
-		OpenedSpriteX: 12 * config.SpriteWidth,
-		OpenedSpriteY: 0,
-	})
-	e.AddComponent(&component.AppearanceComponent{
-		SpriteX:  11 * config.SpriteWidth,
-		SpriteY:  0,
-		Resource: "map",
-	})
-	e.AddComponent(&rlcomponents.DescriptionComponent{Name: "Door"})
+	e, err := factory.Create("door", x, y)
+	if err != nil {
+		panic("spawnDoor: " + err.Error())
+	}
+	e.GetComponent("Position").(*component.PositionComponent).SetPosition(x, y, z)
 	l.Level.AddEntity(e)
 }
 
-// spawnMaintenanceDoor is the same as spawnDoor but uses maintenance tunnel sprites
+// spawnMaintenanceDoor is like spawnDoor but uses maintenance tunnel sprites
 // and sets the underlying tile to maintenance tunnel floor.
 func spawnMaintenanceDoor(l *world.Level, x, y, z int) {
 	l.SetTileTypeAt(x, y, z, world.TypeMaintenanceTunnelFloor)
-
-	e := &ecs.Entity{}
-	pc := &rlcomponents.PositionComponent{}
-	pc.SetPosition(x, y, z)
-	e.AddComponent(pc)
-	e.AddComponent(&rlcomponents.SolidComponent{})
-	e.AddComponent(&rlcomponents.DoorComponent{
-		ClosedSpriteX: 18 * config.SpriteWidth,
-		ClosedSpriteY: 0,
-		OpenedSpriteX: 18 * config.SpriteWidth,
-		OpenedSpriteY: 0,
-	})
-	e.AddComponent(&component.AppearanceComponent{
-		SpriteX:  18 * config.SpriteWidth,
-		SpriteY:  0,
-		Resource: "map",
-	})
-	e.AddComponent(&rlcomponents.DescriptionComponent{Name: "Maintenance Door"})
+	e, err := factory.Create("maintenance_door", x, y)
+	if err != nil {
+		panic("spawnMaintenanceDoor: " + err.Error())
+	}
+	e.GetComponent("Position").(*component.PositionComponent).SetPosition(x, y, z)
 	l.Level.AddEntity(e)
 }
